d342774ede976801aebb7173aaf5e2722b14c270
Nate Zhang <nate@h109.csg.ci.i.u-tokyo.ac.jp>
2015-10-24 14:26:10 +0900

    Initial commit.

---
+++ Assets/Scripts/CamScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CamScript : MonoBehaviour {
+
+	public RawImage image;
+
+	// Use this for initialization
+	void Start () {
+
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		string backCamName = "";
+		for (int i = 0; i < devices.Length; i++) {
+			Debug.Log ("Device " + i +  ":" + devices [i].name + "IS FRONT FACING:" + devices [i].isFrontFacing);
+			if (!devices[i].isFrontFacing) {
+				backCamName = devices[i].name;
+			}
+		}
+
+		WebCamTexture webCamTexture = new WebCamTexture (backCamName, 1280, 720, 30);
+
+
+		image.texture = webCamTexture;
+		image.material.mainTexture = webCamTexture;
+		webCamTexture.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
