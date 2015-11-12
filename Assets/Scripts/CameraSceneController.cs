564e208a80a6d25ee9d6525bb59416a3d72e0940
natezhang <natezhang@outlook.com>
2015-11-12 09:25:59 +0900

    screenshot test

--- Assets/Scripts/CameraSceneController.cs
+++ Assets/Scripts/CameraSceneController.cs
@@ -8,10 +8,6 @@ public class CameraSceneController : MonoBehaviour {
 //	public OVRCameraRig currentCameraRig;
 	private Canvas canvas;
 
-	// oculus position and rotation
-//	private Vector3 position;
-//	private Quaternion rotation;
-
 	private const float DISTANCE_TO_CAMERA = 300.0f;
 
 
@@ -19,19 +15,13 @@ public class CameraSceneController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-//		position = InputTracking.GetLocalPosition (VRNode.CenterEye);
-//		rotation = GetInputTrackingLocalRotation();
 		canvas = GetComponent<Canvas> ();
 		offset = canvas.transform.position - GetInputTrackingLocalRotation();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		Debug.Log ("local position: " + InputTracking.GetLocalPosition (VRNode.CenterEye));
-		Debug.Log ("local rotation: " + InputTracking.GetLocalRotation (VRNode.CenterEye).eulerAngles);
 		canvas.transform.position = GetInputTrackingLocalPosition() + offset;
-//		canvas.transform.rotation = GetInputTrackingLocalRotation();
 	}
 
 	Vector3 GetInputTrackingLocalRotation()
