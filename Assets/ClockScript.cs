6fc549777e660485e1e3dfb1c29915bdca602c61
harihitode <hitode@is.s.u-tokyo.ac.jp>
2015-12-16 22:16:48 +0900

    #28 feature update pictures

--- Assets/ClockScript.cs
+++ Assets/ClockScript.cs
@@ -2,19 +2,30 @@
 using UnityEngine.UI;
 using System.Collections;
 using System;
+using System.Globalization; 
 
 public class ClockScript : MonoBehaviour {
-	public Text txt;
+	public Image i1;
+	public Image i2;
+	public Image i3;
+	public Image i4;
+	private string tm;
 	DateTime dt;
 	// Use this for initialization
-	void Start () {
+	void Start () {	
 		dt = DateTime.Now;
-		txt.text = dt.ToString ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		dt = DateTime.Now;
-		txt.text = dt.ToString ();
+		if (tm != dt.ToString ()) {
+			string strDate = dt.ToString("T", DateTimeFormatInfo.InvariantInfo);		
+			i1.sprite = Resources.Load <Sprite>(strDate[3].ToString ());
+			i2.sprite = Resources.Load <Sprite>(strDate[4].ToString ());
+			i3.sprite = Resources.Load <Sprite>(strDate[6].ToString ());
+			i4.sprite = Resources.Load <Sprite>(strDate[7].ToString ());
+			tm = dt.ToString ();
+		}
 	}
 }
