46332db476402af76b206ecad6f13cf636907568
ganmacs <ganmacs@gmail.com>
2015-12-16 14:56:13 +0900

    use simpleHandSignRecognizer
    To remove dupcalited code

--- Assets/Scripts/Recognizer/RecognizerInterface.cs
+++ Assets/Scripts/Recognizer/RecognizerInterface.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Recognizer
 {
-	interface RecognizerInterface
+	public interface RecognizerInterface
 	{
 		void invokeIfRecognized(Frame frame);
 	}
---
+++ Assets/Scripts/Recognizer/SimpleHandSignRecognizer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Leap;
+using UnityEngine;
+
+namespace Recognizer
+{
+	public abstract class SimpleHandSignRecognizer : RecognizerInterface
+	{
+		const float nextDuration = 3.0f;
+		private float totalTime = 3.0f; // To invoke at first
+		private Action e;
+
+		public abstract bool isRecognized (Frame frame);
+
+		public SimpleHandSignRecognizer(Action e) {
+			this.e = e;
+		}
+
+		public void invokeIfRecognized(Frame frame)
+		{
+			addDeltaTime ();
+
+			if (totalTime > nextDuration && isRecognized (frame)) {
+				resetTotalTime();
+				invoke (frame);
+			}
+		}
+
+		private void resetTotalTime()  {
+			totalTime = 0.0f;
+		}
+
+		private void addDeltaTime() {
+			totalTime += Time.deltaTime;
+		}
+
+		private void invoke(Frame frame)
+		{
+			e ();
+		}
+	}
+}
