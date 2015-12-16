934cab10c1aa4f92a2f30f997c4558905526d098
harihitode <hitode@is.s.u-tokyo.ac.jp>
2015-12-16 15:33:59 +0900

    #28 add pictures and clockscript (olny text)

---
+++ Assets/ClockScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class ClockScript : MonoBehaviour {
+	public Text txt;
+	DateTime dt;
+	// Use this for initialization
+	void Start () {
+		dt = DateTime.Now;
+		txt.text = dt.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dt = DateTime.Now;
+		txt.text = dt.ToString ();
+	}
+}
