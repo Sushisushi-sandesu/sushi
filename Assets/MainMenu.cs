802492fb62c244ef50d88b37fb2e749953518517
neetp7k9 <b98901136@ntu.edu.tw>
2016-01-07 21:12:33 +0900

    line v1

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,11 +3,23 @@ using System;
 using System.Globalization; 
 using System.Collections;
 using Recognizer;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
 	public UnityEngine.UI.Image pictureR;
 	public UnityEngine.UI.Image pictureL;
 
+	TcpListener list;
+	IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+	string remoteAddr = "127.0.0.1";
+	int port2 = 5000;
+	int port1 = 5005;
+
+	bool spaceEnable = true;
 	struct message {
 		public int gazou;
 		public bool you;
@@ -37,15 +49,28 @@ public class MainMenu : MonoBehaviour {
 		tsr = new ThumbsUpRecognizer(() => { Application.LoadLevel ("CameraScene"); });
 		messages = new System.Collections.Generic.Stack<message>(){};
 
+		list = new TcpListener(localAddr,port1);
+		list.Start();
+		Thread listener = new Thread(receiveMessage);
+		listener.Start();
 	}
-
+	void disableSpace(){
+		System.Threading.Thread.Sleep (200);
+		spaceEnable = true;
+	}
 	void showImages () {
+		if (spaceEnable) {
+			spaceEnable = false;
+			Thread exeOnce = new Thread (disableSpace);
+			exeOnce.Start ();
+		} else {
+			return;
+		}
 		if(messages.Count > 0){
 			message last = messages.Peek();
 			int gazou = last.gazou;
 			pictureL.sprite = Resources.Load <Sprite>(selectGazou(gazou));
 			pictureR.sprite = Resources.Load <Sprite>(selectGazou(gazou));
-			linemode = !linemode;
 			pictureL.enabled = linemode;
 			pictureR.enabled = linemode;
 		}
@@ -57,13 +82,42 @@ public class MainMenu : MonoBehaviour {
 		tsr.InvokeIfRecognized (frame);
 
 		if (Input.GetKey(KeyCode.Space)) {
-			showImages();
-
+			linemode = !linemode;
 		} else if (Input.GetKey(KeyCode.A)){
 			message t = new message();
 			t.gazou = UnityEngine.Random.Range (0, 4);
 			t.you   = true;
 			messages.Push(t);
 		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			if(messages.Count > 0){
+				message last = messages.Peek();
+				sendMessage(last);
+			}
+		}
+		showImages();
+	}
+
+	private void receiveMessage(){
+		string rd;
+		while (true) {
+			TcpClient client = list.AcceptTcpClient();
+			StreamReader sr = new StreamReader(client.GetStream());
+			rd = sr.ReadLine();
+			Debug.Log(rd);
+			linemode = true;
+			message t = new message();
+			t.gazou = int.Parse(rd);
+			t.you   = false;
+			messages.Push(t);
+			client.Close ();
+		}
+	}
+	private void sendMessage(message msg) {
+		TcpClient client = new TcpClient(remoteAddr, port2);
+		StreamWriter sw = new StreamWriter(client.GetStream());
+		sw.WriteLine (msg.gazou);
+		sw.Flush();
+		client.Close();
 	}
 }
